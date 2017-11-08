8c71a5999c6f9df66604fefd1eed5478543c77d2
Stian <stiansorslett@hotmail.com>
2017-11-08 17:31:33 +0100

    code

--- hoved.go
+++ hoved.go
@@ -1,20 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+var url = "https://api.fixer.io/latest"
+
+type fromFixer struct {
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	Rates map[string]float32 `json:"rates"`
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+var getShit fromFixer
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	//Fetch response form url1
+
+	repo, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer repo.Body.Close()
+
+	//Grab body from Response
+
+	body, err := ioutil.ReadAll(repo.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &getShit)
+	if err != nil {
+		panic(err)
+	}
+
+	json.NewEncoder(res).Encode(getShit)
 }
