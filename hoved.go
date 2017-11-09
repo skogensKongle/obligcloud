1124c3a67dbb685c73fa0fd10ee12550eaa6f9b4
Stian <stiansorslett@hotmail.com>
2017-11-09 20:27:47 +0100

    not alot of stuff

--- hoved.go
+++ hoved.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
@@ -67,8 +68,8 @@ func main() {
 	router.HandleFunc("/average", handlerAver).Methods("POST")
 
 	fmt.Println("listening...")
-	//err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
-	err := http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
+	//err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		panic(err)
 	}
